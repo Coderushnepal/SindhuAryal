5d2c81d684731863ad8ce4be4dccda3fd2926fd7
Iamsindhu <csitan.sindhu@gmail.com>
2020-07-23 13:54:36 +0545

    Adding position:relative to box

--- Javascript/Assign- 3/script2.js
+++ Javascript/Assign- 3/script2.js
@@ -1,23 +1,26 @@
 const btn = document.getElementById('btn');
 const box = document.getElementById('box');
+//console.log(box);
 
-// const height = box.clientHeight;
-// const width = box.clientWidth;
+const height = box.clientHeight;
+const width = box.clientWidth;
 // const margin = window.innerHeight - height;
-var direction = 1;
 
 btn.addEventListener('click', function(){
     var dot = document.createElement('div');
-    // dot.classList.add('circle');
+    dot.classList.add('circle');
+    console.log(dot.style.height);
+	console.log(dot.style.width);
                 //or
-     dot.style.height = "25px";
-     dot.style.width = "25px";
-     dot.style.borderRadius = "50%";
-     dot.style.backgroundColor = "blue";
-     dot.style.position = "absolute";
+    //  dot.style.height = "25px";
+    //  dot.style.width = "25px";
+    //  dot.style.borderRadius = "50%";
+    //  dot.style.backgroundColor = "blue";
+    //  dot.style.position = "absolute";
 
-    dot.style.top = Math.random()* (box.clientHeight - 30) + 'px';
-    dot.style.left = Math.random()* (box.clientWidth - 30) + 'px';
+    dot.style.top = Math.random()* (height - dot.style.height) + 'px';
+    dot.style.left = Math.random()* (width - dot.style.width) + 'px';
+    // console.log(dot.style.top);
 
     box.appendChild(dot);
 });
