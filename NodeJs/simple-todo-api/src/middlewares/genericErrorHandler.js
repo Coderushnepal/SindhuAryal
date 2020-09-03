2a477f81644e5af8aa46703da395e43850b7ff07
Iamsindu <csitan.sindhu@gmail.com>
2020-09-03 19:55:40 +0545

    add controller, services and error handling middleware

--- NodeJs/simple-todo-api/src/middlewares/genericErrorHandler.js
+++ NodeJs/simple-todo-api/src/middlewares/genericErrorHandler.js
@@ -1,10 +1,11 @@
 import HttpStatusCodes from 'http-status-codes';
+
 import NotFoundError from '../utils/NotFoundError';
 import BadRequestError from '../utils/BadRequestError';
 import UnauthorizedError from '../utils/UnauthorizedError';
 
 export default function genericErrorHandler(err, req, res, next) {
-  if (err.isJoi) {
+  if(err.isJoi) {
     res.status(HttpStatusCodes.BAD_REQUEST).json({
       code: HttpStatusCodes.BAD_REQUEST,
       message: err.name,
@@ -12,34 +13,34 @@ export default function genericErrorHandler(err, req, res, next) {
     })
   }
 
-  if (err instanceof NotFoundError) {
+  if(err instanceof NotFoundError) {
     res.status(HttpStatusCodes.NOT_FOUND).json({
       code: HttpStatusCodes.NOT_FOUND,
       message: err.message
     });
   }
 
-  if (err instanceof BadRequestError) {
+  if(err instanceof BadRequestError) {
     res.status(HttpStatusCodes.BAD_REQUEST).json({
       code: HttpStatusCodes.BAD_REQUEST,
       message: err.message
     });
   }
 
-  if (err instanceof UnauthorizedError) {
+  if(err instanceof UnauthorizedError) {
     res.status(HttpStatusCodes.UNAUTHORIZED).json({
       code: HttpStatusCodes.UNAUTHORIZED,
       message: err.message
     });
   }
 
-
-  const errorMessage = err.message ?
-    err.message
-    :
-    HttpStatusCodes.getStatusText(INTERNAL_SERVER_ERROR);
-
-  if (err) {
+  const errorMessage = 
+    err.message ?
+      err.message
+      :
+      HttpStatusCodes.getStatusText(HttpStatusCodes.INTERNAL_SERVER_ERROR);
+      
+  if(err) {
     res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).json({
       code: HttpStatusCodes.INTERNAL_SERVER_ERROR,
       message: errorMessage
