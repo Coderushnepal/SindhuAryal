0ab040dd118d221b9a640ae334d36a2510a948c7
Iamsindu <csitan.sindhu@gmail.com>
2020-09-07 13:28:37 +0545

    add migration scripts

--- NodeJs/simple-todo-api/src/db.js
+++ NodeJs/simple-todo-api/src/db.js
@@ -1,18 +1,5 @@
 import Knex from 'knex';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const dbConfig = {
-    client : process.env.DB_CLIENT,
-    connection : {
-        host : process.env.DB_HOST,
-        user : process.env.DB_USER,
-        password : process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-        database: process.env.DB_NAME
-    }
-}
+import { dbConfig } from './knexfile';
 
 const connection = Knex(dbConfig);
 
