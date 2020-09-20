d1535c4ac0cf17e85576cb003b078f7ed7d3ced1
Iamsindu <csitan.sindhu@gmail.com>
2020-09-20 17:29:02 +0545

    fetch only blogs where is_active = true(soft delete)

--- final-project/node/src/models/blogs.js
+++ final-project/node/src/models/blogs.js
@@ -4,30 +4,36 @@ import camelize from 'camelize';
 
 const table = 'blogs';
 
+export async function getAll() {
+  const data = await connection
+    .select('id', 'title', 'desc', 'image', 'is_active', 'created_at')
+    .from(table)
+    .where('is_active', true);
+
+  return camelize(data);
+}
+
+export async function getById(id) {
+  const [data] = await connection
+    .select('id', 'title', 'desc', 'image', 'is_active', 'created_at')
+    .from(table)
+    .where({ id: id, is_active: true });
+
+  return data[0] ? camelize(data[0]) : null;
+}
+
 export async function create(params) {
   const [data] = await connection.insert(snakeize(params)).into(table).returning('*');
 
   return camelize(data);
 }
 
-export async function getAll() {
-  const data = await connection.select('*').from(table);
-
-  return camelize(data);
-}
-
-export async function getById(id) {
-  const [data] = await connection.select('*').from(table).where({ id });
-
-  return data ? camelize(data) : null;
+export async function remove(blogsId) {
+  await connection(table).update({ is_active: false }).where({ id: blogsId });
 }
 
 export async function update(blogsId, params) {
   const [updatedData] = await connection(table).update(snakeize(params)).where({ id: blogsId }).returning('*');
 
   return camelize(updatedData);
-}
-
-export async function remove(blogsId) {
-  await connection(table).update({ is_active: false }).where({ id: blogsId });
 }
